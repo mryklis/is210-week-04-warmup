99c5cac132dcbb0e9057b9a1d0900f662662dce5
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-09-17 15:16:19 +0000

    created boolean function

---
+++ task_05.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This function performs logical operations"""
+
+def defaults(my_required, my_optional=True):
+    return my_optional is my_required

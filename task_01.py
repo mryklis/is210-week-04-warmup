21c0d901e69ccc387d0ec39b496b8220d2878e70
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-09-17 14:21:33 +0000

    added function doc string

--- task_01.py
+++ task_01.py
@@ -4,7 +4,23 @@
 
 
 def know_what_i_mean(wink, numwink=2):
-    winks = (wink * numwink).strip()
-    nudges = ('nudge ' * numwink).strip()
-    retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
-    return retstr
+        """Returns a string, replacing the number of winks and nudges.
+
+        Uses format to insert the number of winks and nudges into retstr.
+
+        Args:
+                  wink: string variable consisting of wink repeated by the number of numwink.
+                  numwink: integer variable set to 2
+
+        Returns:
+                  retstr: string variable that knows what I mean
+
+        Example:
+
+                >>> know_what_i_mean('maxim', 2)
+                'Know what I mean? maximmaxim nudge nudge'
+        """
+        winks = (wink * numwink).strip()
+        nudges = ('nudge ' * numwink).strip()
+        retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
+        return retstr
